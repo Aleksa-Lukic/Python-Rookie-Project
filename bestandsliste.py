843d89b063fff2c1e51c2c19f5dc70079464a1f1
Aleksa Lukic <65134116+MOJOMOOOO@users.noreply.github.com>
2022-02-28 05:56:32 +0100

    Add files via upload

---
+++ bestandsliste.py
@@ -0,0 +1,56 @@
+#####  ####
+#
+#
+#
+
+
+from time import gmtime, strftime
+
+datum = strftime("%d.%m.%Y")
+abgabezeit = strftime("%H:%M:%S")
+
+
+#Abgabezeit und Datum
+print(f"\n\t\t \n\tAbgabezeit: \t{abgabezeit}\n\tDatum:\t\t{datum}\n\n")
+print("\tBestandsliste\t\t\t\tHeute Sortiert\n")
+
+print("\t----Container 35³----")
+
+print("\t1. SG1 offen:\t\tWERT\t\t25. SG1 offen:\t\t\tWERT")
+print("\t2. SG1 gedeckelt:\tWERT\t\t26. SG1 gedeckelt:\t\tWERT")
+print("\t3. SG2 offen:\t\tWERT\t\t27. SG2 offen:\t\t\tWERT")
+print("\t4. SG2 gedeckelt:\tWERT\t\t28. SG2 gedeckelt:\t\tWERT")
+print("\t5. SG4 offen:\t\tWERT\t\t29. SG4 offen:\t\t\tWERT")
+print("\t6. SG4 gedeckelt:\tWERT\t\t30. SG4 gedeckelt:\t\tWERT")
+print("\t7. SG5 offen:\t\tWERT\t\t31. SG5 offen:\t\t\tWERT")
+print("\t8. Misch offen:\t\tWERT\t\t32. Misch geleert offen:\tWERT")
+print("\t9. Misch gedeckelt:\tWERT\t\t34. Misch geleert gedeckelt:\tWERT")
+print("\t10. Leer offen:\t\tWERT")
+print("\t11. Leer gedeckelt:\tWERT\n")
+
+
+print("\t---- Gitterboxen - LSB Boxen - Paletten - Photovoltaik Module -----")
+
+print("\t12. Gibos Leer:\t\tWERT\t\t35. Gibos Geleert:\t\tWERT")
+print("\t13. Gibos Batterie:\tWERT")
+print("\t14. Gibos Handy:\tWERT")
+print("\t15. Gibos Reciever:\tWERT")
+print("\t16. Gibos Router:\tWERT")
+print("\t17. Gibos Laptops:\tWERT")
+print("\t18. Gibos Steckernetz:\tWERT")
+print("\t19. Gibos Werkzeuge:\tWERT")
+print("\t20. Gibos Analog:\tWERT")
+print("\t21. Gibos Digital:\tWERT")
+print("\t22. LSB Boxen:\t\tWERT")
+print("\t23. Nachtspeicher:\tWERT")
+print("\t24. Photovoltaik:\tWERT")
+
+
+
+
+
+
+
+
+
+
